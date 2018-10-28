c3ef206d073011dac4bd0f804532319306edcef1
Idris Cheikh <rouchd-idris.cheikh@etu.unige.ch>
2018-10-28 19:31:55 +0100

    The program now works.

--- main.c
+++ main.c
@@ -1,52 +1,19 @@
+
 /* Source: K&R */
+
 #include <stdio.h>
 #include <string.h>
-//#include <unistd.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/syscall.h>
 #include <sys/stat.h>
-#include "dirent.h"
+#include <dirent.h>
+#include <errno.h>
 
-
-//#define NAME_MAX 14
 #define MAX_PATH 1024
 
-typedef struct {
-  long ino;
-  char name[NAME_MAX+1];
-} Dirent;
-
-// typedef struct {
-//   int fd;
-//   Dirent d;
-// } DIR;
-
-//DIR *opendir(char *dirname);
-//Dirent *readdir(DIR *dfd);
-//void closedir(DIR *dfd);
-
-char *name;
-struct stat stbuf;
-//int stat(char *, struct stat *);
 void dirwalk(char *, void (*fcn)(char *));
-
-//stat(name, &stbuf);
-
-// struct stat
-// {
-//   dev_t   st_dev;
-//   ino_t   st_ino;
-//   short   st_mode;
-//   short   st_nlink;
-//   short   st_uid;
-//   short   st_gid;
-//   dev_t   st_rdev;
-//   off_t   st_size;
-//   time_t  st_atime;
-//   time_t  st_mtime;
-//   time_t  st_ctime;
-// };
-
 void fsize(char *);
 
 void fsize(char *name)
@@ -54,6 +21,7 @@ void fsize(char *name)
   struct stat stbuf;
 
   if (stat(name, &stbuf) == -1) {
+    fprintf(stdout, "yo %s\n",strerror( errno )); // hem hem
     fprintf(stderr, "fsize: can't access %s\n", name);
     return;
   }
@@ -65,7 +33,7 @@ void fsize(char *name)
 void dirwalk(char *dir, void (*fcn)(char *))
 {
   char name[MAX_PATH];
-  Dirent *dp;
+  struct dirent *dp;
   DIR *dfd;
 
   if ((dfd = opendir(dir)) == NULL) {
@@ -73,12 +41,12 @@ void dirwalk(char *dir, void (*fcn)(char *))
     return;
   }
   while ((dp = readdir(dfd)) != NULL) {
-    if (strcmp(dp->name, ".") == 0 || strcmp(dp->name, "..") == 0)
+    if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
       continue;
-    if (strlen(dir)+strlen(dp->name)+2 > sizeof(name))
-      fprintf(stderr, "dirwalk: name %s/%s too long\n", dir, dp->name);
+    if (strlen(dir)+strlen(dp->d_name)+2 > sizeof(name))
+      fprintf(stderr, "dirwalk: name %s/%s too long\n", dir, dp->d_name);
     else {
-      sprintf(name, "%s/%s", dir, dp->name);
+      sprintf(name, "%s/%s", dir, dp->d_name);
       (*fcn)(name);
     }
   }
